c092c372d09cd7c6c8261c434e207c01cae813d1
lastnamearya <arya.jigyasu6815@gmail.com>
2019-07-13 19:44:47 +0530

    Array.from() also create a shallow copy so dont' use it in referencing state ~ array in React

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
-// Shallow copy using SPREAD Syntax
+// Array.from ~ Again problem with Shallow Copy.
 
-var obj = { foo: "foo", bar: "bar" };
+// We created a state Object that has nested array inside.
 
-var cloneObj = { ...obj };
+var state = {
+  array: [{ a: 1 }, { b: 2 }, { c: 3 }, { d: 4 }]
+};
 
-// Shallow copy using Object.assign
+// Now we created an new array based on state's array using Array.from( ). It'll create a new shallow copy instance.
 
-var clonedObj = Object.assign({}, obj);
+const newArray = Array.from(state.array);
 
-console.log(clonedObj);
+// currently first element is { a: 1}
+
+console.log(newArray[0]);
+
+// Now we updated the first element of array inside the original state object.
+
+state.array[0] = { a: "One" };
+
+// It'll return ~ {a: "One"} which was previously { a: 1 }
+
+console.log(state.array[0]);
+
+// Now here the Problem ~ if we check our instance that we created above to see if it reflects new changes or not then we found ~ { a: 1 }. It won't reflect changes so it's a bad practice to have a state reference using Array.from().
+
+console.log(newArray[0]);
