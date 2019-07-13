ea54930085ad5f3f7abaeaa551c3d8e461106ea6
lastnamearya <arya.jigyasu6815@gmail.com>
2019-07-14 00:54:31 +0530

    Shallow copy ~ gotcha with reassignment of reference index

--- app.js
+++ app.js
@@ -1,27 +1,49 @@
-// Array.from ~ Again problem with Shallow Copy.
+// Shallow copy in Nested Arrays.
 
-// We created a state Object that has nested array inside.
+var numbers = [1, 2, 3];
 
-var state = {
-  array: [{ a: 1 }, { b: 2 }, { c: 3 }, { d: 4 }]
-};
+var numbersCopy = [...numbers];
 
-// Now we created an new array based on state's array using Array.from( ). It'll create a new shallow copy instance.
+console.log(numbersCopy);
 
-const newArray = Array.from(state.array);
+numbersCopy.push(4);
 
-// currently first element is { a: 1}
+// It'll  be [1, 2, 3, 4]
 
-console.log(newArray[0]);
+console.log(numbersCopy);
 
-// Now we updated the first element of array inside the original state object.
+// Here see one thing we pushed value to above numberCopy Array not to the numbers array so it won't share that reference with numbers
 
-state.array[0] = { a: "One" };
+// It'll log ~ [1, 2, 3]
 
-// It'll return ~ {a: "One"} which was previously { a: 1 }
+console.log(numbers);
 
-console.log(state.array[0]);
+// *********************************************** //
 
-// Now here the Problem ~ if we check our instance that we created above to see if it reflects new changes or not then we found ~ { a: 1 }. It won't reflect changes so it's a bad practice to have a state reference using Array.from().
+// Here things started becoming weird.
 
-console.log(newArray[0]);
+var nestedNumbers = [[1], [2]];
+
+// Now we created a shallow copy of nestedNumbers
+
+var nestedNumbersCopy = [...nestedNumbers];
+
+// It'll remain same as nestedNumbers
+
+console.log(nestedNumbersCopy);
+
+// Now we reassigned the value of nestedNumbersCopy array ~ 0 index value
+
+nestedNumbersCopy[0].push(300);
+
+// Now our nestedNumbersCopy will be [ [1, 300], [2]]
+
+console.log(nestedNumbersCopy);
+
+// Here's the gotcha our shallow copy shares the reference of our original array from where we created this copy so we still had the exact same index reference and that also going to point nestedNumbers.
+
+// So automatically our nestedNumbers is also going to  be update ~ It'll become [[1, 300], [2]]
+
+// The key concept to understand is we pushed or reassigned the reference index not did something new in memory allocation. That's the gotcha.
+
+console.log(nestedNumbers);
